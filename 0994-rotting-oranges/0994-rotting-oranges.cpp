1f60f01cc51a0d10ee5a26697c7392dadbd0d3b8
Dev Popli <d1e1v1234@gmail.com>
2026-07-02 15:21:10 +0530

    Time: 2 ms (32.23%), Space: 16.7 MB (86.27%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                    q.push({i,j});
+                else if(grid[i][j]==1)
+                    fresh++;
+            }
+        }
+        if(fresh==0)
+            return 0;
+
+        int minutes = 0;
+
+        vector<pair<int,int>> dir = {
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1}
+        };
+
+        while(!q.empty())
+        {
+            int sz = q.size();
+            bool rotten = false;
+
+            while(sz--)
+            {
+                auto [x,y] = q.front();
+                q.pop();
+
+                for(auto d : dir)
+                {
+                    int nx = x + d.first;
+                    int ny = y + d.second;
+
+                    if(nx>=0 && nx<n && ny>=0 && ny<m &&
+                       grid[nx][ny]==1)
+                    {
+                        grid[nx][ny]=2;
+                        fresh--;
+                        rotten=true;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+
+            if(rotten)
+                minutes++;
+        }
+
+        return fresh==0 ? minutes : -1;
+    }
+};
