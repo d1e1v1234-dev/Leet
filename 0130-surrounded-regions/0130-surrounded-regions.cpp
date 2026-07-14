2fa31593df2890b710ddb73cd294269aaf40eb51
Dev Popli <d1e1v1234@gmail.com>
2026-07-14 22:59:55 +0530

    Time: 0 ms (100%), Space: 14.6 MB (50.56%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -13,42 +13,41 @@ public:
         vector<vector<int>> vis(n,vector<int>(m,0));
         for(int i=0;i<m;i++)
         {
-            if(board[0][i]=='O') dfs(board,0,i,vis,n,m);
+            if(board[0][i]=='O') dfs(board,0,i,n,m);
         }
         for(int i=1;i<n;i++)
         {
-            if(board[i][0]=='O') dfs(board,i,0,vis,n,m);
+            if(board[i][0]=='O') dfs(board,i,0,n,m);
         }
         for(int i=1;i<m;i++)
         {
-            if(board[n-1][i]=='O') dfs(board,n-1,i,vis,n,m);
+            if(board[n-1][i]=='O') dfs(board,n-1,i,n,m);
         }
         for(int i=1;i<n-1;i++)
         {
-            if(board[i][m-1]=='O') dfs(board,i,m-1,vis,n,m);
+            if(board[i][m-1]=='O') dfs(board,i,m-1,n,m);
         }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] && board[i][j]=='O') board[i][j]='X';
-                vis[i][j]=0;
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='#') board[i][j]='O';
             }
         }
     }
-    void dfs(vector<vector<char>>& board , int i , int j,vector<vector<int>>& vis,int& n , int& m)
+    void dfs(vector<vector<char>>& board , int i , int j,int& n , int& m)
     {
-        if(vis[i][j]) return;
 
-        vis[i][j] = 1;
+        board[i][j] = '#';
         for(auto d: dir)
         {
             int nx = i + d.first;
             int ny = j + d.second;
 
-            if(nx>=0 && nx<n && ny>=0 && ny<m && board[nx][ny]=='O' && !vis[nx][ny])
+            if(nx>=0 && nx<n && ny>=0 && ny<m && board[nx][ny]=='O')
             {
-                dfs(board,nx,ny,vis,n,m);
+                dfs(board,nx,ny,n,m);
             }
         }
     }
