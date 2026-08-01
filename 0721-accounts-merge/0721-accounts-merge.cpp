dd2812c6054bde9739ace69bc349864588fca4c4
Dev Popli <d1e1v1234@gmail.com>
2026-08-01 22:20:39 +0530

    Time: 34 ms (50.27%), Space: 39.7 MB (83.36%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,54 +1,83 @@
+class DSU {
+    public:
+    vector<int>parent,rank;
+    
+    DSU(int n)
+    {
+        parent.resize(n);
+        rank.assign(n,0);
+        for(int i=0;i<n;i++)
+        {
+            parent[i] =i;
+        }   
+    }
+    int find(int x)
+    {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    void Union(int x,int y)
+    {
+        int px = find(x);
+        int py = find(y);
+        if(px == py) return;
+        if(rank[px] < rank[py])
+        {
+            parent[px] = py;
+        }
+        else if(rank[px] > rank[py])
+        {
+            parent[py] = px;
+        }
+        else
+        {
+            parent[py] = px;
+            rank[px]++;
+        }
+    }
+};
+
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) 
     {
         int n = accounts.size();
-        vector<bool> vis(n,false);
-        vector<vector<string>> ans;
+        DSU dsu(n);
+        unordered_map<string,int> emailToAccount;
         for(int i=0;i<n;i++)
         {
-            if(vis[i]) continue;
-            set<string> s;
-            dfs(accounts,vis,i,n,s);
-            vector<string> v;
-            v.push_back(accounts[i][0]);
-            for(auto& email : s)
+            for(int j = 1;j<accounts[i].size();j++)
             {
-                v.push_back(email);
+                string email = accounts[i][j];
+
+                if(emailToAccount.count(email))
+                {
+                    dsu.Union(i,emailToAccount[email]);
+                }
+                else
+                {
+                    emailToAccount[email] = i;
+                }
             }
-            ans.push_back(v);
+        }
+        unordered_map<int,vector<string>> merged;
+        for(auto& it : emailToAccount)
+        {
+            string email = it.first;
+            int account = it.second;
+            int root = dsu.find(account);
+            merged[root].push_back(email);
+        }
+        vector<vector<string>> ans;
+        for(auto& it : merged)
+        {
+            int root = it.first;
+            vector<string> emails = it.second;
+            sort(emails.begin(),emails.end());
+            emails.insert(emails.begin(),accounts[root][0]);
+            ans.push_back(emails);
         }
         return ans;
-    }
-    void dfs(vector<vector<string>>& accounts,vector<bool>& vis , int idx ,int& n , set<string>& s )
-    {
-        vis[idx] =true;
-        for(int i=1;i<accounts[idx].size();i++)
-        {
-            s.insert(accounts[idx][i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            bool found = false;
-            int x = accounts[i].size();
-            if(!vis[i] && accounts[i][0] == accounts[idx][0] )
-            {
-                
-                for(int k=1;k<x;k++)
-                {
-                    if(s.find(accounts[i][k]) != s.end()) found = true;
-                    if(found) break;
-                }
-            }
-            if(found)
-            {
-                vis[i]=true;
-                for(int k=1;k<x;k++)
-                {
-                    s.insert(accounts[i][k]);
-                }
-                dfs(accounts,vis,i,n,s);
-            }
-        }
+
     }
 };
