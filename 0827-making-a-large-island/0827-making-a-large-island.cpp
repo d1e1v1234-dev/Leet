dcca930c8563dcd671ca60e0bf658a50e24146bc
Dev Popli <d1e1v1234@gmail.com>
2026-08-03 23:13:14 +0530

    Time: 267 ms (49.58%), Space: 159.5 MB (33.06%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,85 +1,112 @@
+class DSU {
+public:
+    vector<int> parent, sz;
+
+    DSU(int n) {
+        parent.resize(n);
+        sz.assign(n, 1);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int u, int v) {
+        u = find(u);
+        v = find(v);
+
+        if (u == v)
+            return;
+
+        if (sz[u] < sz[v])
+            swap(u, v);
+
+        parent[v] = u;
+        sz[u] += sz[v];
+    }
+
+    int size(int x) {
+        return sz[find(x)];
+    }
+};
+
 class Solution {
 public:
     vector<pair<int,int>> dir = {
-            {1,0},
-            {-1,0},
-            {0,-1},
-            {0,1}
+        {1,0},
+        {-1,0},
+        {0,1},
+        {0,-1}
+    };
+
+    int largestIsland(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        DSU dsu(n * n);
+
+        auto id = [&](int i, int j) {
+            return i * n + j;
         };
-    int largestIsland(vector<vector<int>>& grid) 
-    {
-        int n = grid.size();
-        int ans =0;
-        vector<pair<int,int>> zeroes;
-        unordered_map<int,int> colors;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j] == 0) zeroes.push_back({i,j});
-            }
-        }
-        int color=2;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]== 1)
-                {
-                    grid[i][j] = color;
-                    int count =1;
-                    dfs(grid,i,j,count,color);
-                    colors[color] =count;
-                    color++;
-                }
-            }
-        }
-        if(zeroes.empty())
-            return n * n;
-        for(auto& it: zeroes)
-        {
-            int i = it.first;
-            int j = it.second;
-            int res = 0;
-            unordered_set<int> s;
-            
-            for(auto d : dir)
-            {
-                int nx = i + d.first;
-                int ny = j + d.second;
-                if(nx>=0 && nx<n && ny>=0 && ny<n )
-                {
-                int v = grid[nx][ny];
-                if(v == 0)continue;
-                
-                if(s.find(v) == s.end())
-                {
-                    s.insert(v);
-                    res+=colors[v];
-                }
-                }
-            }
-            res++;
-            ans = max(res,ans);
-        }
-        return ans;
-    }
 
-    
-    void dfs(vector<vector<int>>& grid , int i , int j , int&count ,int& color )
-    {
-        int n = grid.size();
-        for(auto d: dir)
-        {
-            int nx = i + d.first;
-            int ny = j + d.second;
+        // Build islands
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
-            if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]== 1)
-            {
-                count++;
-                grid[nx][ny] =color;
-                dfs(grid,nx,ny,count,color);
+                if (grid[i][j] == 0)
+                    continue;
+
+                // right
+                if (j + 1 < n && grid[i][j + 1] == 1)
+                    dsu.unite(id(i,j), id(i,j+1));
+
+                // down
+                if (i + 1 < n && grid[i + 1][j] == 1)
+                    dsu.unite(id(i,j), id(i+1,j));
             }
         }
+
+        int ans = 0;
+        bool hasZero = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == 1) {
+                    ans = max(ans, dsu.size(id(i,j)));
+                    continue;
+                }
+
+                hasZero = true;
+
+                unordered_set<int> roots;
+                int area = 1;
+
+                for (auto d : dir) {
+
+                    int ni = i + d.first;
+                    int nj = j + d.second;
+
+                    if (ni < 0 || nj < 0 || ni >= n || nj >= n)
+                        continue;
+
+                    if (grid[ni][nj] == 0)
+                        continue;
+
+                    int root = dsu.find(id(ni,nj));
+
+                    if (roots.insert(root).second)
+                        area += dsu.sz[root];
+                }
+
+                ans = max(ans, area);
+            }
+        }
+
+        return hasZero ? ans : n * n;
     }
 };
