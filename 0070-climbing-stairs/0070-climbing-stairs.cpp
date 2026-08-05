0430736acef370e5230352bb6af6941bc9e7c04e
Dev Popli <d1e1v1234@gmail.com>
2026-08-05 22:25:06 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,15 +3,15 @@ public:
     
     int climbStairs(int n) 
     {
-        if(n==1 || n ==2) return n;
-        vector<int> dp(n+1);
-        dp[1] = 1; 
-        dp[2] = 2; 
+        if(n==1 || n==2) return n;
+        int prev1 = 1,prev2 = 2;
         for(int i=3;i<=n;i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            int prev3 = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = prev3;
         }    
-        return dp[n];
+        return prev2;
     }
     
 };
