b2dec7bbd1c2b77aa995018a0eb81a93eb611bf5
Dev Popli <d1e1v1234@gmail.com>
2026-08-06 17:52:26 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,18 +2,32 @@ class Solution {
 public:
     int rob(vector<int>& nums) 
     {
-        int n = nums.size();
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[1],nums[0]);
-        if(n==3) return max(nums[0] + nums[2],nums[1]);
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        dp[2] = nums[0]+nums[2];
-        for(int i = 3 ; i<n;i++)
+        int prev1=0,prev2=0;
+        for(int x: nums)    
         {
-            dp[i] = max(dp[i-2],dp[i-3]) + nums[i];
+            int curr = max(prev1,prev2+x);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return max(dp[n-1],dp[n-2]);
+        return max(prev1,prev2);
     }
 };
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) 
+//     {
+//         int n = nums.size();
+//         if(n==1) return nums[0];
+//         if(n==2) return max(nums[1],nums[0]);
+//         if(n==3) return max(nums[0] + nums[2],nums[1]);
+//         vector<int> dp(n);
+//         dp[0] = nums[0];
+//         dp[1] = nums[1];
+//         dp[2] = nums[0]+nums[2];
+//         for(int i = 3 ; i<n;i++)
+//         {
+//             dp[i] = max(dp[i-2],dp[i-3]) + nums[i];
+//         }
+//         return max(dp[n-1],dp[n-2]);
+//     }
+// };
